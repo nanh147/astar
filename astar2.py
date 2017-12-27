9f4d8963ca28f328d18ea2413cd34adb398a3bf2
Richard Arthurs <rarthurs@sfu.ca>
2017-12-26 16:13:41 -0800

    working plot of obstacles and solution path

--- astar2.py
+++ astar2.py
@@ -24,16 +24,13 @@ class AStar(object):
         self.grid_height = 6;
         self.grid_width = 6;
 
-    def init_grid(self, start, end):
-        obstacles = ((0, 5), (1, 0), (1, 1), (1, 5), (2, 3),
-             (3, 1), (3, 2), (3, 5), (4, 1), (4, 4), (5, 1))
-
+    def init_grid(self, start, end, obstacles):
         for x in range(self.grid_width):
             for y in range(self.grid_height):
                 if (x, y) in obstacles:
-                    obstacle = True
-                else:
                     obstacle = False
+                else:
+                    obstacle = True
                 self.nodes.append(Node(x, y, obstacle))
 
         self.start = self.get_node(*start)
@@ -64,7 +61,8 @@ class AStar(object):
 
     def show_path(self):
         node = self.end
-        path = [node.x, node.y]
+        path = []
+        path.append((node.x, node.y))
         while node.parent is not self.start:
             node = node.parent
             path.append((node.x, node.y))
@@ -76,8 +74,8 @@ class AStar(object):
 
     def update_node(self, nextNode, node):
         # updates the next node
-        nextNode.g = self.heuristic(nextNode, self.start)#node.g + 10 #self.heuristic(nextNode, self.start)
-        nextNode.h = self.heuristic(nextNode, self.end) #self.heuristic2(nextNode) #self.heuristic(nextNode, self.end)
+        nextNode.g = node.g + 10#node.g + 10 #self.heuristic(nextNode, self.start)
+        nextNode.h = self.heuristic2(nextNode) #self.heuristic2(nextNode) #self.heuristic(nextNode, self.end)
         nextNode.f = nextNode.g + nextNode.h
         nextNode.parent = node
 
@@ -92,10 +90,10 @@ class AStar(object):
 
             neighbours = self.get_neighbours(node) # grab the neighbours
             for neighbour in neighbours:
-                if ~neighbour.obstacle and neighbour not in self.closed:
+                if neighbour.obstacle and neighbour not in self.closed:
                     if (neighbour.f, neighbour) in self.open:
                     # if neighbour is in open list, check if current path is better than the one previously found for this neighbour
-                        if neighbour.g > node.g: # previously: > node.g + 10
+                        if neighbour.g > node.g + 10: # previously: > node.g + 10
                             self.update_node(neighbour, node)
                     else:
                         self.update_node(neighbour, node)
@@ -109,12 +107,21 @@ if __name__ == '__main__':
     # plt.plot(x,y)
     # plt.axis([0, 20, 0, 30])
     # plt.show()
+    obstacles = ((0, 5), (1, 0), (1, 1), (1, 5), (2, 3),
+     (3, 1), (3, 2), (3, 5), (4, 1), (4, 4), (5, 1))
+
+
     astar = AStar()
-    astar.init_grid([0,0],[5,5])
+    astar.init_grid([0,0],[5,5], obstacles)
     result = astar.process()
 
     x, y = zip(*result)
-    plt.scatter(*zip(*result))
+    xobs, yobs = zip(*obstacles)
+
+    plt.axis([-1, 10, -1, 10])
+    plt.plot(x,y, 'gd')
+    plt.plot(xobs, yobs, 'kx')
+
 
     plt.show()
 
